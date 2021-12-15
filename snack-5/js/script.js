dc60365488b8c3da6461c7b0e88335e821715bfc
SpaceWolfy <s_scozzari@libero.it>
2021-12-15 16:35:52 +0100

    comment added - snack 5 completed

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,4 +1,4 @@
-
+//Aggiungere numeri ad un array vuoto finché la somma di questi ultimi non superi il 50:
 let arrayNum = [];
 let sum = 0;
 let num;
@@ -15,10 +15,10 @@ do {
 
     if(sum >= 50) {
         alert('Attento! Ti avevo detto di non superare il 50')
-        console.log('Cavolo... non dovevi superare il 50. La tua somma è', sum)
+        console.log('Cavolo... non dovevi superare il 50. La tua somma è', sum);
     } else if(sum < 50) {
-        console.log('Perfetto! non hai superato il limite. La tua somma è', sum)
+        console.log('Perfetto! non hai superato il limite. La tua somma è', sum);
     }
-} while (sum <= 49)
+} while (sum <= 49);
 
 
